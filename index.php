b3e0f01576ad266a9930b75977f99a841f8e59ab
ppsn <alhaitham@tuta.io>
2023-08-15 17:02:07 +0300

    first steps, no interaction with database

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Torque</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <nav>
+        <div class="divider"></div>
+        <a href="newsale.php">New Sale</a>
+        <form action="search.php" method="get">
+            <input type="text" placeholder="Search" name="q">
+            <input type="submit">
+        </form>
+    </nav>
+</body>
+</html>
---
+++ newsale.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Sale</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] != "POST") {
+            $url = "http://localhost:3000/sql";
+            $curl = curl_init($url);
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            
+
+            $carname = $_POST["carname"];
+            $new_carname = str_replace(" ", "+", $carname);
+            $new_carname = "carname=".$new_carname;
+
+            $email = $_POST["email"];
+            $new_email = "email=".$email;
+
+            $phone = $_POST["phone"];
+            $new_phone = "phone=".$phone;
+
+            $extrainfo = $_POST["extrainfo"];
+            $new_extrainfo = str_replace(" ", "+", $extrainfo);
+            $new_extrainfo = "carname=".$new_extrainfo;
+
+            $postfields = $new_carname."&".$new_email."&".$new_phone."&".$new_extrainfo;
+
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $postfields);
+    
+            $headers = array(
+                "NS: torque",
+                "DB: torque",
+                "Content-Type: application/json",
+                "X-Custom-Header: header-value"
+            );
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($curl, CURLOPT_HEADER, true);
+            curl_setopt($curl, CURLOPT_USERPWD, "root:root");
+            $response = curl_exec($curl);
+            curl_close($curl);
+            echo $response;
+        }
+    ?>
+    <form action="newsale.php" method="post" id="saleform">
+        <table>
+            <tr>
+                <td style="text-align: center;">
+                    <a href="index.php">Back</a>
+                </td>
+                <th>
+                    <label for="saleform">New Sale</label>
+                </th>
+            </tr>
+            <tr>
+                <th>
+                    <label for="carname">Car Name</label>
+                </th>
+                <td>
+                    <input type="text" name="carname" id="carname" required placeholder="Car Name">
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    <label for="email">Seller E-mail</label>
+                </th>
+                <td>
+                    <input type="email" name="email" id="email" required placeholder="E-mail Address">
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    <label for="phone">Seller Phone Number</label>
+                </th>
+                <td>
+                    <input type="tel" name="phone" id="phone" required placeholder="Phone Number">
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    <label for="extrainfo">Extra Information</label>
+                </th>
+                <td>
+                    <textarea name="extrainfo" id="extrainfo" cols="29" rows="10" 
+                    placeholder="Extra Information"></textarea>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type="submit">
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
