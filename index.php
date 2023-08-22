cfd0476435aa1a10c8e1ffccb6a68e30582c0d3f
ppsn <alhaitham@tuta.io>
2023-08-22 16:20:19 +0300

    fixed listings display in main page

--- index.php
+++ index.php
@@ -7,22 +7,13 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <nav>
-        <div class="divider"></div>
-        <a href="newsale.php">New Sale</a>
-        <form action="search.php" method="get">
-            <input type="text" placeholder="Search" name="q">
-            <input type="submit">
-        </form>
-    </nav>
-    <?php 
+<?php 
         $url = "http://localhost:3000/sql";
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-        $userquery = "SELECT * FROM car";
-
+        $userquery = "SELECT * FROM car ORDER BY RAND()";
         curl_setopt($curl, CURLOPT_POSTFIELDS, $userquery);
 
         $headers = array(
@@ -31,12 +22,42 @@
             "Accept: application/json",
         );
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($curl, CURLOPT_HEADER, true);
+        curl_setopt($curl, CURLOPT_HEADER, false);
         curl_setopt($curl, CURLOPT_USERPWD, "root:root");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
         $response = curl_exec($curl);
+        $response = json_decode($response);
+        $response = $response[0];
         curl_close($curl);
-        //$response = json_decode($response);
-        echo $response;
     ?>
+    <nav>
+        <a href="newsale.php">New Sale</a>
+        <form action="search.php" method="get">
+            <input type="text" placeholder="Search" name="q">
+            <input type="submit">
+        </form>
+    </nav>
+    <main>
+        <p>
+            Loaded in <?php echo $response->time?>
+        </p>
+
+        <h1>
+            Listings
+            <!--  ?> -->
+            <?php 
+                $listings = $response->result;
+                //var_dump($listings);
+                foreach ($listings as $listing) {
+                    echo "<a href='/sale.php?id=".$listing->id."'><h1>".$listing->carname."</h1></a>";
+                }
+            ?>
+        </h1>
+    </main>
+    <?php var_dump($listings)?>
+
+    
+
 </body>
 </html>
