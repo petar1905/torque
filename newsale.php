204a46b996799b76f7605355bc41c140e64186f7
ppsn <alhaitham@tuta.io>
2023-08-22 16:19:44 +0300

    centered new sale

--- newsale.php
+++ newsale.php
@@ -34,55 +34,59 @@
             echo $response;
         }
     ?>
-    <form action="newsale.php" method="post" id="saleform">
-        <table>
-            <tr>
-                <td style="text-align: center;">
-                    <a href="index.php">Back</a>
-                </td>
-                <th>
-                    <label for="saleform">New Sale</label>
-                </th>
-            </tr>
-            <tr>
-                <th>
-                    <label for="carname">Car Name</label>
-                </th>
-                <td>
-                    <input type="text" name="carname" id="carname" required placeholder="Car Name">
-                </td>
-            </tr>
-            <tr>
-                <th>
-                    <label for="email">Seller E-mail</label>
-                </th>
-                <td>
-                    <input type="email" name="email" id="email" required placeholder="E-mail Address">
-                </td>
-            </tr>
-            <tr>
-                <th>
-                    <label for="phone">Seller Phone Number</label>
-                </th>
-                <td>
-                    <input type="tel" name="phone" id="phone" required placeholder="Phone Number">
-                </td>
-            </tr>
-            <tr>
-                <th>
-                    <label for="extrainfo">Extra Information</label>
-                </th>
-                <td>
-                    <textarea name="extrainfo" id="extrainfo" cols="29" rows="10" 
-                    placeholder="Extra Information"></textarea>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2">
-                    <input type="submit">
-                </td>
-            </tr>
-        </table>
-    </form>
+    <section id="form-container">
+        <div class="spacer"></div>
+        <form action="newsale.php" method="post" id="saleform">
+            <table>
+                <tr>
+                    <td style="text-align: center;">
+                        <a href="index.php">Back</a>
+                    </td>
+                    <th>
+                        <label for="saleform">New Sale</label>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <label for="carname">Car Name</label>
+                    </th>
+                    <td>
+                        <input type="text" name="carname" id="carname" required placeholder="Car Name">
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label for="email">Seller E-mail</label>
+                    </th>
+                    <td>
+                        <input type="email" name="email" id="email" required placeholder="E-mail Address">
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label for="phone">Seller Phone Number</label>
+                    </th>
+                    <td>
+                        <input type="tel" name="phone" id="phone" required placeholder="Phone Number">
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label for="extrainfo">Extra Information</label>
+                    </th>
+                    <td>
+                        <textarea name="extrainfo" id="extrainfo" cols="29" rows="10" 
+                        placeholder="Extra Information"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <input type="submit">
+                    </td>
+                </tr>
+            </table>
+        </form>
+        <div class="spacer"></div>
+    </section>
 </body>
 </html>
